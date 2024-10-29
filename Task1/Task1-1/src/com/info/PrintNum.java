3114553355cf76c06f44603a5a69fdf902fea4cc
SuryaBhavani-Venigalla <suryabhavanivenigalla@gmail.com>
2024-10-29 12:03:45 +0530

    Add files via upload

---
+++ Task1/Task1-1/src/com/info/PrintNum.java
@@ -0,0 +1,14 @@
+package com.info;
+
+public class PrintNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=10; i<=50; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ Task1/Task1-1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module PrintNumbers {
+}
